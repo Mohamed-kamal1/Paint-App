ffec52d172a16441bbd2b0fd94ce0246cdaf6e38
mohamedKamal <https://github.com/Mohamed-kamal1>
2022-12-10 20:53:02 +0200

    update

--- Backend/Paint/src/main/java/com/example/Paint/Controller.java
+++ Backend/Paint/src/main/java/com/example/Paint/Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/back")
 public class Controller {
     @GetMapping("/shape")
-    public void control (@RequestParam long id, @RequestParam String type, @RequestParam String fill, @RequestParam String border, @RequestParam long[] coordinates){
+    public void control (@RequestParam long id, @RequestParam String type, @RequestParam String fill, @RequestParam String border, @RequestParam double[] coordinates){
 
         ShapeFactory factory =new ShapeFactory();
         Shape shape=factory.createShape(type);
